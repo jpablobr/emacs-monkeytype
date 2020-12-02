c72bbf6dfb6e802bbb6f966b0a4a47cf607d138e
jpablobr <xjpablobrx@gmail.com>
2020-12-02 05:58:44 -0600

    Update Commentary section

--- monkeytype.el
+++ monkeytype.el
@@ -18,10 +18,6 @@
 
 ;; - Type any text you want.
 
-;; - Practice mistyped words.
-
-;; - UI customisation.
-
 ;; - Mode-line live WPM (`monkeytype-mode-line-interval-update' adjust the
 ;; update frequency).
 
@@ -59,22 +55,26 @@
 ;; - `monkeytype-word-regexp' customises the regexp used for removing characters
 ;; from words (defaults to: ;:.\`",()-?!).
 
+;; - Ability to type most (saved) mistyped words (the amount of words is
+;; configurable with `monkeytype-most-mistyped-amount' [defaults to 100]) see:
+;; `monkeytype-most-mistyped-words'
+
 ;;; License:
 
 ;; This file is NOT part of GNU Emacs.
 ;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 2 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 2 of the License, or (at your option) any later
+;; version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Code:
 
